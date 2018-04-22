572d13b08665b2da65309550a857dc74017cbfeb
SaiShilpa Dasari <sdasari@corp.miraclesoft.com>
2018-04-22 06:17:08 +0530

    ganesh first to commit to jenkins-tf

---
+++ main.tf
@@ -0,0 +1,96 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+##################################################################
+# Data sources to get VPC, subnet, security group and AMI details
+##################################################################
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name = "name"
+
+    values = [
+      "amzn-ami-hvm-*-x86_64-gp2",
+    ]
+  }
+
+  filter {
+    name = "owner-alias"
+
+    values = [
+      "amazon",
+    ]
+  }
+}
+
+# Master Server
+resource "aws_instance" "ec2_jenkins_master" {
+ name                        = "example"
+  ami                         = "${data.aws_ami.amazon_linux.id}"
+  instance_type               = "t2.micro"
+  subnet_id                   = "${element(data.aws_subnet_ids.all.ids, 0)}"
+  vpc_security_group_ids      = ["${module.security_group.this_security_group_id}"]
+  associate_public_ip_address = true
+
+  lifecycle {
+      ignore_changes = ["what should be the value of this?"]
+  }
+
+  provisioner "file" {
+    connection  = {
+      user = "ec2-user"
+      private_key = "${file(var.ssh_key_path)}"
+    }
+    content     = "${var.setup_data}"
+    destination = "/tmp/setup.sh"
+  }
+
+  provisioner "remote-exec" {
+    connection  = {
+      user = "ec2-user"
+      private_key = "${file(var.ssh_key_path)}"
+    }
+    inline = [
+      "chmod +x /tmp/setup.sh",
+      "sudo /tmp/setup.sh"
+    ]
+  }
+}
+module "security_group" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "example"
+  description = "Security group for example usage with EC2 instance"
+  vpc_id      = "${data.aws_vpc.default.id}"
+
+  http_port = "$8080"
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "all-icmp"]
+  egress_rules        = ["all-all"]
+}
+
+resource "aws_eip" "this" {
+  vpc      = true
+  instance = "${module.ec2.id[0]}"
+}
+
+module "ec2" {
+  source = "../../"
+
+  name                        = "example"
+  ami                         = "${data.aws_ami.amazon_linux.id}"
+  instance_type               = "t2.micro"
+  subnet_id                   = "${element(data.aws_subnet_ids.all.ids, 0)}"
+  vpc_security_group_ids      = ["${module.security_group.this_security_group_id}"]
+  associate_public_ip_address = true
+}
